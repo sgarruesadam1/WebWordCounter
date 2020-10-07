730deebf5b51f83b82cc832d8fb8f17775623904
sgarruesadam1 <sgarruesadam1@gmail.com>
2020-10-07 20:03:31 +0200

    Proyecto Base

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace WebWordCounter
+{
+    public class Program
+    {
+        public static XXXXXXXXXX Task Main()
+        {
+            string url = null;
+            string w = null;
+            int n = 0;
+
+            Console.WriteLine("WEB WORD COUNTER\n");
+            Console.WriteLine("Programa que cuenta\ncuántas veces aparece\nen una web que se indique\nla palabra que se quiera.\n");
+
+            while(true)
+            {
+                Console.WriteLine("Escribe la url o exit:");
+                url = Console.ReadLine();
+                if (String.Equals(url.Trim().ToUpper(),"EXIT")) break;
+                if (!url.StartsWith("http")) continue;
+
+                Task<string[]> task1 = Task.Run(() => CreateWordArray(url));
+
+                Console.WriteLine("Escribe la palabra:");
+                w = Console.ReadLine();
+                
+                string[] words = task1.XXXXXXXXXX;                
+                
+                XXXXXXXXXX Task.Run(() => {
+                    n = GetCountForWord(words, w);
+                });
+                
+                Console.WriteLine("La palabra aparece {0} veces.", n);
+            }
+
+            Console.WriteLine("FIN\n");
+
+        }
+
+        private static int GetCountForWord(string[] words, string term)
+        {
+            var findWord = from word in words
+                           where word.ToUpper().Contains(term.ToUpper())
+                           select word;
+            return findWord.Count();
+        }
+
+        static string[] CreateWordArray(string uri)
+        {
+            string s = new WebClient().DownloadString(uri);
+            return s.Split(
+                new char[] { ' ', '\u000A', ',', '.', ';', ':', '-', '_', '/', '\\', '<', '>', '(', ')', '"', '\'', '&', '»' },
+                StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
